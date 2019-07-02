371dd95b04dac9ea69614ac40f10f610c609756a
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-07-02 09:52:08 -0600

    initial commit

---
+++ PokedexObjC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokedexObjC
+//
+//  Created by Madison Kaori Shino on 7/2/19.
+//  Copyright © 2019 Madi S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PokedexObjC/Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PokedexObjC
+//
+//  Created by Madison Kaori Shino on 7/2/19.
+//  Copyright © 2019 Madi S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
